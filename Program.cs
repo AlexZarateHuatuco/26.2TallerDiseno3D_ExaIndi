95796006b74d1a46a51d130f33ed192746417ed0
Alex Zarate <azarate45@gmail.com>
2026-09-24 17:18:15 -0500

    Creación Proyecto

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallerDiseno3D_ExaIndi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
